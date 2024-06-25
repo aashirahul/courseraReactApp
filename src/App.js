fd8ad3e4543ef875a9edac9421b2b8f67bccfaa8
Ashima <ashima@Rahuls-MacBook-Pro.local>
2024-06-25 09:12:06 -0400

    added navigation and routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Header from './components/Header';
 import InputComponent from './components/InputComponent';
 import Sidebar from './components/Sidebar';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
  return( <div>
   <Header name="Anna" color="pink"/>
-  <Sidebar />
+  <nav>
+    <Link to="/" className="nav-item">InputComponent</Link>
+    <Link to="/sidebar" className="nav-item"> Sidebar</Link>
+  </nav>
   <div></div>
-  <InputComponent />
+  <Routes>
+    <Route path ="/" element = {<InputComponent />} />  
+    <Route path ="/sidebar" element = { <Sidebar />} />  
+  </Routes>
  </div>
  
 
